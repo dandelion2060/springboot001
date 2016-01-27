1267a2b74431caf0be974405df8906ed596c2f7f
dandelion2060 <ray_dandelion@foxmail.com>
2016-01-28 00:37:15 +0800

    aaqweqwe

--- src/main/java/com/ray/utils/iputil/IPUtils.java
+++ src/main/java/com/ray/utils/iputil/IPUtils.java
@@ -3,5 +3,9 @@ package com.ray.utils.iputil;
 public class IPUtils {
 	
 	private static final String IPURL="";
+	public static void main(String args[]){
+		System.out.print("1212");
+		System.out.println("sdfsdf");
+	}
 
 }
