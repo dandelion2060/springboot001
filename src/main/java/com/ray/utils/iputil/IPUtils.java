b86b6a59df9dffe9f8c8d5a3a3c90deeff96592c
HuaSheng <ray_dandelion@foxmail.com>
2016-04-26 14:01:18 +0800

    commit

--- src/main/java/com/ray/utils/iputil/IPUtils.java
+++ src/main/java/com/ray/utils/iputil/IPUtils.java
@@ -4,6 +4,10 @@ public class IPUtils {
 	
 	private static final String IPURL="";
 	public static void main(String args[]){
+		int i = 0;
+		for(;i<10;i++){
+			System.out.println(i);
+		}
 		System.out.print("1212");
 		System.out.println("sdfsdf");
 	}
